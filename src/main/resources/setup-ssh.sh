7ee6478f3f1970fffa4ecaeeb53b10d7c959f079
Jenkins Automation Server <jenkins@example.com>
2025-02-11 12:02:41 +0900

    setup-ssh.sh

--- src/main/resources/setup-ssh.sh
+++ src/main/resources/setup-ssh.sh
@@ -5,11 +5,11 @@ mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
 # SSH Key 설정
-echo "$MONGO_SSH_KEY" > /root/.ssh/mongodb-key.pem
-chmod 400 /root/.ssh/mongodb-key.pem
+echo "$MONGO_SSH_KEY" > /root/.ssh/raku-key.pem
+chmod 400 /root/.ssh/raku-key.pem
 
 # SSH 포트 포워딩 설정
-ssh -i /root/.ssh/mongodb-key.pem -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -N -L 27017:127.0.0.1:27017 ec2-user@${MONGO_HOST} &
+ssh -i /home/ec2-user/raku-key.pem -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -N -L 27017:127.0.0.1:27017 ec2-user@${MONGO_HOST} &
 
 echo "✅ SSH Tunnel established. Starting application..."
 exec java -jar /app.jar
